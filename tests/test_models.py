70b34cad0b0a31d9229d0ace8a53db0103b839e4
Pavel Tanchev <dcopm999@gmail.com>
2020-06-19 20:01:24 +0500

    tests.test_models: Added some unittests.

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,17 +9,38 @@ Tests for `crm-push` models module.
 """
 
 from django.test import TestCase
+from django.urls import reverse
 
 from push import models
 
 
-class TestPush(TestCase):
+class PushTestCase(TestCase):
+    fixtures = ['initial_data.json']
 
     def setUp(self):
+        self.entry = models.Push.objects.last()
+
+    def test_push_str(self):
+        self.assertEqual(self.entry.title, self.entry.__str__())
+
+    def test_absolute_url(self):
+        self.assertEqual(self.entry.get_absolute_url(), reverse('push:push-update', kwargs={'pk': self.entry.pk}))
+
+    def tearDown(self):
         pass
 
-    def test_something(self):
-        pass
+
+class OptionTestCase(TestCase):
+    fixtures = ['initial_data.json']
+
+    def setUp(self):
+        self.entry = models.Option.objects.last()
+
+    def test_push_str(self):
+        self.assertEqual(self.entry.name, self.entry.__str__())
+
+    def test_absolute_url(self):
+        self.assertEqual(self.entry.get_absolute_url(), reverse('push:option-update', kwargs={'pk': self.entry.pk}))
 
     def tearDown(self):
         pass
