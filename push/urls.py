c1c2f19fbaf19e3ccaa083b21a56262c8668dd41
Pavel Tanchev <dcopm999@gmail.com>
2020-06-19 19:40:26 +0500

    push.urls: adding routes

--- push/urls.py
+++ push/urls.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import url
-from django.views.generic import TemplateView
+from django.urls import path
 
-from . import views
-
+from push import views
 
 app_name = 'push'
+
 urlpatterns = [
-    url(r'', TemplateView.as_view(template_name="base.html")),
-    ]
+    # PUSH
+    path('push/', views.PushListView.as_view(), name="push-list"),
+    path('push/create/', views.PushCreateView.as_view(), name="push-create"),
+    path('push/update/<int:pk>', views.PushUpdateView.as_view(), name="push-update"),
+    path('push/delete/<int:pk>', views.PushDeleteView.as_view(), name="push-delete"),
+    # OPTION
+    path('option/', views.OptionListView.as_view(), name="option-list"),
+    path('option/create/', views.OptionCreateView.as_view(), name="option-create"),
+    path('option/update/<int:pk>', views.OptionUpdateView.as_view(), name="option-update"),
+    path('option/delete/<int:pk>', views.OptionDeleteView.as_view(), name="option-delete"),
+]
